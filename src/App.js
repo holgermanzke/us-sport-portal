4859a6d28ce74e5bc1a9716bea1c326bf97d241b
holgermanzke <manzkeholger@gmail.com>
2019-11-22 12:20:44 +0100

    Add title to header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import GlobalStyles from "./components/GlobalStyles";
 import AppBar from "./components/Header";
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <AppBar>
-        <h1>US Sports App</h1>
-      </AppBar>
+
+      <AppBar />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Title from "./Title";
 
 const AppBar = styled.header`
   background: #2b2758;
   height: 80px;
+  display: flex;
 `;
 
 export default function Header() {
-  return <AppBar />;
+  return (
+    <AppBar>
+      <Title text="Bundesliga DB"></Title>
+    </AppBar>
+  );
 }
