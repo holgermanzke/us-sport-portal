4eef0fd3fb3f4da000ae930e394b145b4c0dc649
holgermanzke <manzkeholger@gmail.com>
2019-11-22 11:53:30 +0100

    Add an appbar to header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const AppBar = styled.header`
+  background: #2b2758;
+  height: 80px;
+`;
+
+export default function Header() {
+  return <AppBar />;
+}
